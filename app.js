b428bbe6b1422f332b664ddc9c018d561e7a5d47
XboxInterns <thexinterns@outlook.com>
2017-06-02 11:31:58 -0700

    basic mixer chatbot outline
    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const XClient = require('beam-client-node');
+const XSocket = require('beam-client-node/lib/ws');
+
+let userInfo;
+
+const client = new XClient();
+
+// With OAuth we don't need to login, the OAuth Provider will attach
+// the required information to all of our requests after this call.
+client.use('oauth', {
+    tokens: {
+        access: 'cUbq78omtrfgWtulBqSjCFRz1b3BlWAIomoMlVK6aND7wYuCSgIQNWo5hsNcWkKD',
+        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
+    },
+});
+
+// Get's the user we have access to with the token
+client.request('GET', `users/current`)
+    .then(response => {
+        userInfo = response.body;
+        return client.chat.join(response.body.channel.id);
+    })
+    .then(response => {
+        const body = response.body;
+        return createChatSocket(userInfo.id, userInfo.channel.id, body.endpoints, body.authkey);
+    })
+    .catch(error => {
+        console.log('Something went wrong:', error);
+    });
+
+/**
+ * Creates a beam chat socket and sets up listeners to various chat events.
+ * @param {number} userId The user to authenticate as
+ * @param {number} channelId The channel id to join
+ * @param {any} endpoints An endpoints array from a beam.chat.join call.
+ * @param {any} authkey An authentication key from a beam.chat.join call.
+ * @returns {Promise.<>}
+ */
+function createChatSocket (userId, channelId, endpoints, authkey) {
+    // Chat connection
+    const socket = new XSocket(endpoints).boot();
+
+    // Greet a joined user
+    socket.on('UserJoin', data => {
+        socket.call('msg', [`Hi ${data.username}! I'm pingbot! Write !ping and I will pong back!`]);
+    });
+
+    // React to our !pong command
+    socket.on('ChatMessage', data => {
+        if (data.message.message[0].data.toLowerCase().startsWith('!ping')) {
+            socket.call('msg', [`@${data.user_name} PONG!`]);
+            console.log(`Ponged ${data.user_name}`);
+        }
+    });
+
+    // Handle errors
+    socket.on('error', error => {
+        console.error('Socket error', error);
+    });
+
+    return socket.auth(channelId, userId, authkey)
+        .then(() => {
+            console.log('Login successful');
+            return socket.call('msg', ['Hi! I\'m pingbot! Write !ping and I will pong back!']);
+        });
+}
