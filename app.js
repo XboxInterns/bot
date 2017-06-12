26c9cfa9509a9351d1c2975fcf13cdc3b56ec19c
XboxInterns <thexinterns@outlook.com>
2017-06-12 16:28:29 -0700

    dad jokes

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const XClient = require('beam-client-node');
-const XSocket = require('beam-client-node/lib/ws');
+const XClient = require('beam-client-node')
+const XSocket = require('beam-client-node/lib/ws')
+var fs = require('fs');
+let userInfo
 
-let userInfo;
-
-const client = new XClient();
+const client = new XClient()
 
 // With OAuth we don't need to login, the OAuth Provider will attach
 // the required information to all of our requests after this call.
 client.use('oauth', {
-    tokens: {
-        access: 'kn7IfCBlsHASvATqRwhkap2y82cpKO2nSNBVMBl5ww5XfCb2KjlNDEGdgFR2vGe0',
-        expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
-    },
-});
+  tokens: {
+    access: 'kn7IfCBlsHASvATqRwhkap2y82cpKO2nSNBVMBl5ww5XfCb2KjlNDEGdgFR2vGe0',
+    expires: Date.now() + (365 * 24 * 60 * 60 * 1000)
+  },
+})
 
 // Get's the user we have access to with the token
-//my dudes does this work
 client.request('GET', `users/current`)
-    .then(response => {
-        userInfo = response.body;
-        //before: response.userInfo.channelId
-        //now: hardcoded xboxinterns channelId
-        return client.chat.join(6725743);
-    })
-    .then(response => {
-        const body = response.body;
-        //channel id has to be xboxinterns - hardcoded
-        return createChatSocket(userInfo.id, 6725743, body.endpoints, body.authkey);
-    })
-    .catch(error => {
-        console.log('Something went wrong:', error);
-    });
+  .then(response => {
+    userInfo = response.body
+    //before: response.userInfo.channelId
+    //now: hardcoded xboxinterns channelId
+    return client.chat.join(6772196)
+  })
+  .then(response => {
+    const body = response.body
+    //channel id has to be xboxinterns - hardcoded
+    return createChatSocket(userInfo.id, 6772196, body.endpoints, body.authkey)
+  })
+  .catch(error => {
+    console.log('Something went wrong:', error)
+  })
 
 /**
  * Creates a beam chat socket and sets up listeners to various chat events.
@@ -41,53 +40,62 @@ client.request('GET', `users/current`)
  * @returns {Promise.<>}
  */
 function createChatSocket (userId, channelId, endpoints, authkey) {
-    // Chat connection
-    const socket = new XSocket(endpoints).boot();
+  // Chat connection
+  const socket = new XSocket(endpoints).boot()
 
-    // Greet a joined user
-    socket.on('UserJoin', data => {
-        socket.call('msg', [`Hi ${data.username}! I'm pingbot! Write !ping and I will pong back!`]);
-    });
+  // Greet a joined user
+  socket.on('UserJoin', data => {
+    socket.call('msg', [`Hi ${data.username}! I'm pingbot! Write !ping and I will pong back!`])
+  })
 
-    // React to our !pong command
-    socket.on('ChatMessage', data => {
-        // input is an array of the strings given by user, seperated by spaces
-        var input = data.message.message[0].data.split(" ");
-       // console.log(input[0]);
-        if (input[0] == '!ping') {
-            socket.call('msg', [`@${data.user_name} PONG!`]);
-            console.log(`Ponged ${data.user_name}`);
-        }
+  // React to our !pong command
+  socket.on('ChatMessage', data => {
+    // input is an array of the strings given by user, seperated by spaces
+    var input = data.message.message[0].data.split(' ')
+    // console.log(input[0]);
+    if (input[0] == '!ping') {
+      socket.call('msg', [`@${data.user_name} PONG!`])
+      console.log(`Ponged ${data.user_name}`)
+    }
 
-      if (input[0] == '!spin' && isNaN(input[1])) {
-        socket.call('msg', [`@${data.user_name} please enter a valid number`]);
+    if (input[0] == '!spin' && isNaN(input[1])) {
+      socket.call('msg', [`@${data.user_name} please enter a valid number`])
+    }
+
+    if (input[0] == '!spin' && !isNaN(input[1])) {
+      // result = a number between -input[1] and +input[1]
+      if (Math.random() > 0) {          // 50/50 chance of winning
+        socket.call('msg', [`@${data.user_name} won ${input[1]} points!`])
+      } else {
+        socket.call('msg', [`@${data.user_name} lost...`])
       }
+    }
 
-       if (input[0] == '!spin' && !isNaN(input[1])) {
-        // result = a number between -input[1] and +input[1]
-        if (Math.random() > Math.random()) {
-            console.log(input[1]);
-            var winnings = (Math.random() *  parseInt(input[1])) + parseInt(input[1]);
-            socket.call('msg', [`@${data.user_name} won ${Math.round(winnings)} points!`]);
-        } else {
-            socket.call('msg', [`@${data.user_name} lost the spin...`]);
-        }
-      }
+    if(input[0] == '!dadjoke') {
+      randomLine('/Users/t-anpark/Desktop/bot/dadjokes.txt')
+      socket.call('msg', [`@${data.user_name} ${line}`])
+    }
+  })
 
+  // Handle errors
+  socket.on('error', error => {
+    console.error('Socket error', error)
+  })
 
-    });
+  return socket.auth(channelId, userId, authkey)
+    .then(() => {
+      console.log('Login successful')
+      return socket.call('msg', ['Hi! I\'m pingbot! Write !ping and I will pong back!'])
+    })
+}
 
-
-
-    // Handle errors
-    socket.on('error', error => {
-        console.error('Socket error', error);
-    });
-
-    return socket.auth(channelId, userId, authkey)
-        .then(() => {
-            console.log('Login successful');
-            return socket.call('msg', ['Hi! I\'m pingbot! Write !ping and I will pong back!']);
-        });
+var line;
+function randomLine(fileName) {
+  fs.readFile(fileName, function(err, data) {
+    if (err) throw err;
+    data += ' '
+    var lines = data.split('\n')
+    line = lines[Math.floor(Math.random()*lines.length)];
+  })
 }
 
