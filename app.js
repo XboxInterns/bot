9851ac84e7f6e37f9deac57fe8aab732b41bf69f
XboxInterns <thexinterns@outlook.com>
2017-06-20 12:25:11 -0700

    semicolons

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const XClient = require('beam-client-node')
-const XSocket = require('beam-client-node/lib/ws')
-const Carina = require('carina').Carina
-const ws = require('ws')
-Carina.WebSocket = ws
-const ca = new Carina({isBot: true}).open()
-const channelId = 6772196
-let fs = require('fs')
-const readline = require('readline')
-let userInfo
-let qSet = new Set()
-const client = new XClient()
-let jokes
-let commands
+const XClient = require('beam-client-node');
+const XSocket = require('beam-client-node/lib/ws');
+const Carina = require('carina').Carina;
+const ws = require('ws');
+Carina.WebSocket = ws;
+const ca = new Carina({isBot: true}).open();
+const channelId = 6772196;
+let fs = require('fs');
+const readline = require('readline');
+let userInfo;
+let qSet = new Set();
+const client = new XClient();
+let jokes;
+let commands;
 // With OAuth we don't need to login, the OAuth Provider will attach
 // the required information to all of our requests after this call.
 client.use('oauth', {
@@ -24,10 +24,10 @@ client.use('oauth', {
 // Get's the user we have access to with the token
 client.request('GET', `users/current`)
   .then(response => {
-    userInfo = response.body
+    userInfo = response.body;
     //before: response.userInfo.channelId
     //now: hardcoded xboxinterns channelId
-    return client.chat.join(channelId)
+    return client.chat.join(channelId);
   })
   .then(response => {
     const body = response.body
@@ -35,7 +35,7 @@ client.request('GET', `users/current`)
     return createChatSocket(userInfo.id, channelId, body.endpoints, body.authkey)
   })
   .catch(error => {
-    console.log('Something went wrong:', error)
+    console.log('Something went wrong:', error);
   })
 
 /**
@@ -48,20 +48,20 @@ client.request('GET', `users/current`)
  */
 function createChatSocket (userId, channelId, endpoints, authkey) {
   // Chat connection
-  const socket = new XSocket(endpoints).boot()
+  const socket = new XSocket(endpoints).boot();
 
   ca.subscribe(`channel:${channelId}:followed`, data => {
     if (data.following) {
-      socket.call('msg', [`@${data.user.username} followed!`])
+      socket.call('msg', [`@${data.user.username} followed!`]);
     }
   })
 
   ca.subscribe(`channel:${channelId}:hosted`, data => {
-    socket.call('msg', [`@${data.user.username} began hosting our channel! Thanks!`])
+    socket.call('msg', [`@${data.user.username} began hosting our channel! Thanks!`]);
   })
 
   ca.subscribe(`channel:${channelId}:subscribed`, data => {
-    socket.call('msg', [`@${data.user.username} subscribed!`])
+    socket.call('msg', [`@${data.user.username} subscribed!`]);
   })
 
   // Greet a joined user
@@ -72,118 +72,109 @@ function createChatSocket (userId, channelId, endpoints, authkey) {
   // React to our !pong command
   socket.on('ChatMessage', data => {
     // input is an array of the strings given by user, seperated by spaces
-    let input = data.message.message[0].data.split(' ')
-    let opt = input[0].toLowerCase()
+    let input = data.message.message[0].data.split(' ');
+    let opt = input[0].toLowerCase();
 
     if (!commands) {
-      commands = new Array
-      generateCommandsMap('./commands.txt')
+      commands = new Array;
+      generateCommandsMap('./commands.txt');
     }
 
     if (commands[opt]) {
       socket.call('msg', [commands[opt]])
-      console.log(commands[opt])
+      console.log(commands[opt]);
     }
 
     if (opt === '!create' && input[1] && input[2]) {
       if (!commands[input[1]]) {
-        commands[input[1]] = input[2]
+        commands[input[1]] = input[2];
         fs.appendFile('./commands.txt', `\r${input[1]}+${input[2]}`, function (err) {
-          if (err) return console.log(err)
+          if (err) return console.log(err);
         })
       }
     }
 
-    // console.log(input[0]);
-    if (opt === '!ping') {
-      socket.call('msg', [`@${data.user_name} PONG!`])
-      console.log(`Ponged ${data.user_name}`)
-    }
-
-    if (opt === '!spin' && isNaN(input[1])) {
-      socket.call('msg', [`@${data.user_name} please enter a valid number`])
-    }
 
 
     let ret;
     switch (opt) {
       case '!spin':
         if (isNaN(input[1])) {
-          ret = 'please enter a valid number'
+          ret = 'please enter a valid number';
         } else {
           if (Math.random() > Math.random()) {
-            let winnings = Math.round((Math.random() * parseInt(input[1])) + parseInt(input[1]))
-            ret = 'won ' + winnings + ' points!'
+            let winnings = Math.round((Math.random() * parseInt(input[1])) + parseInt(input[1]));
+            ret = 'won ' + winnings + ' points!';
           } else {
-            ret = 'lost the spin...'
+            ret = 'lost the spin...';
           }
         }
         break;
       case '!coinflip':
         if (input[1] && (input[1].toLowerCase() !== 'heads' || input[1].toLowerCase() !== 'tails')) {
           if (Math.random() > 0.5) {
-            ret = 'HEADS!'
+            ret = 'HEADS!';
           } else {
-            ret = 'TAILS!'
+            ret = 'TAILS!';
           }
-          ret = 'flips a coin... The result of the coin flip is: ' + ret
+          ret = 'flips a coin... The result of the coin flip is: ' + ret;
         } else {
-          ret = 'please enter "heads" or "tails"'
+          ret = 'please enter "heads" or "tails"';
         }
         break;
       case '!dieroll':
-        ret = 'rolls a die... The result of the die roll is: ' + (Math.floor(Math.random() * 6) + 1)
+        ret = 'rolls a die... The result of the die roll is: ' + (Math.floor(Math.random() * 6) + 1);
         break;
       case '!dadjoke':
         if (!jokes) {
-          jokes = generateArray('./dadjokes.txt')
+          jokes = generateArray('./dadjokes.txt');
         }
-        ret = randomLine(jokes)
+        ret = randomLine(jokes);
         break;
     }
     if (ret !== '') {
-      socket.call('msg', [`@${data.user_name} ${ret}`])
+      socket.call('msg', [`@${data.user_name} ${ret}`]);
     }
 
     if (opt === '!q') {
       // below pushes the entire message including the '!q'
-      qSet.add(data.message.message[0].data)
-      getQuestions() // !!! delete this since it'll repeatedly print the questions
+      qSet.add(data.message.message[0].data);
+      // getQuestions() // !!! delete this since it'll repeatedly print the questions
     }
   })
 
   // Handle errors
   socket.on('error', error => {
-    console.error('Socket error', error)
+    console.error('Socket error', error);
   })
 
   return socket.auth(channelId, userId, authkey)
     .then(() => {
-      console.log('Login successful')
-      return socket.call('msg', ['Hi! I\'m InternXBot, Write !commandlist to see my full list of commands!'])
+      console.log('Login successful');
+      return socket.call('msg', ['Hi! I\'m InternXBot, Write !commandlist to see my full list of commands!']);
     })
 }
 
 function generateArray (fileName) {
-  return fs.readFileSync(fileName).toString().split('\n')
+  return fs.readFileSync(fileName).toString().split('\n');
 }
 
 function generateCommandsMap (fileName) {
   fs.readFileSync(fileName).toString().split('\r\n').forEach(function (line) {
-    var temp = line.split('=')
-    commands[temp[0]] = temp[1]
+    var temp = line.split('=');
+    commands[temp[0]] = temp[1];
   })
 }
 
 function randomLine (arr) {
-  let length = arr.length
-  line = arr[Math.floor(Math.random() * length)]
-  return line
+  let length = arr.length;
+  line = arr[Math.floor(Math.random() * length)];
+  return line;
 }
 
 function getQuestions () {
   for (let i of qSet) {
-    console.log(i)
+    console.log(i);
   }
 }
 
